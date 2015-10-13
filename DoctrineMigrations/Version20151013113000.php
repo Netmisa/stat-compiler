737161bc6bd0074e2897a319a5813d8c440a5cb7
David Quintanel <david.quintanel@canaltp.fr>
2015-10-13 14:47:24 +0200

    insert date into coverage_modes

--- DoctrineMigrations/Version20151013113000.php
+++ DoctrineMigrations/Version20151013113000.php
@@ -18,6 +18,7 @@ class Version20151013113000 extends AbstractMigration
         $this->addSql('
             CREATE TABLE stat_compiled.coverage_modes
             (
+              request_date timestamp without time zone,
               region_id text NOT NULL,
               type text NOT NULL,
               mode text NOT NULL,
@@ -25,8 +26,7 @@ class Version20151013113000 extends AbstractMigration
               commercial_mode_name text,
               is_internal_call integer,
               nb integer,
-              request_date timestamp without time zone,
-              CONSTRAINT coverage_modes_pkey PRIMARY KEY (region_id, type, mode, commercial_mode_id, commercial_mode_name, is_internal_call)
+              CONSTRAINT coverage_modes_pkey PRIMARY KEY (request_date, region_id, type, mode, commercial_mode_id, commercial_mode_name, is_internal_call)
             )
             WITH (
               OIDS=FALSE
@@ -45,7 +45,7 @@ class Version20151013113000 extends AbstractMigration
               IF NOT EXISTS(SELECT 1 FROM pg_tables WHERE tablename=partition and schemaname=schema) THEN
                 RAISE NOTICE \'A partition has been created %\',partition;
                 EXECUTE \'CREATE TABLE IF NOT EXISTS \' || schema || \'.\' || partition ||
-                        \' (CONSTRAINT \' || partition || \'_pkey PRIMARY KEY (region_id, type, mode, commercial_mode_id, commercial_mode_name, is_internal_call),
+                        \' (CONSTRAINT \' || partition || \'_pkey PRIMARY KEY (request_date, region_id, type, mode, commercial_mode_id, commercial_mode_name, is_internal_call),
                           check (request_date >= DATE \'\'\' || to_char(NEW.request_date, \'YYYY-MM-01\') || \'\'\'
                                   AND request_date < DATE \'\'\' || to_char(NEW.request_date + interval \'1 month\', \'YYYY-MM-01\') || \'\'\') ) \' ||
                         \'INHERITS (\' || schema || \'.coverage_modes);\';
