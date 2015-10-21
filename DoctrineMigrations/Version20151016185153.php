fe6d60c656a427362572b55669b89ee285dac867
Thomas Noury <thomas.noury@canaltp.fr>
2015-10-21 14:00:37 +0200

    Moved object_count to request_calls table

--- DoctrineMigrations/Version20151016185153.php
+++ DoctrineMigrations/Version20151016185153.php
@@ -16,50 +16,32 @@ class Version20151016185153 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->addSql('
-            CREATE TABLE stat_compiled.response_info
-            (
-                datetime date NOT NULL,
-                user_id integer NOT NULL,
-                object_type character varying(100) NOT NULL,
-                object_count integer,
-                CONSTRAINT pk_response_info PRIMARY KEY (datetime, user_id, object_type)
-            )
-            WITH (
-                OIDS=FALSE
-            );
+            ALTER TABLE stat_compiled.requests_calls ADD COLUMN object_count integer;
         ');
 
         $this->addSql('
-            CREATE OR REPLACE FUNCTION response_info_insert_trigger()
-            RETURNS TRIGGER AS $$
-            DECLARE
+          CREATE OR REPLACE FUNCTION requests_calls_insert_trigger()
+            RETURNS trigger AS $$
+              DECLARE
                 schema VARCHAR(100);
                 partition VARCHAR(100);
-            BEGIN
+              BEGIN
                 schema := \'stat_compiled\';
-                partition := \'response_info\' || \'_\' || to_char(NEW.datetime, \'"y"YYYY"m"MM\');
+                partition := \'requests_calls\' || \'_\' || to_char(NEW.request_date, \'"y"YYYY"m"MM\');
                 IF NOT EXISTS(SELECT 1 FROM pg_tables WHERE tablename=partition and schemaname=schema) THEN
-                    RAISE NOTICE \'A partition has been created %\',partition;
-                    EXECUTE \'CREATE TABLE IF NOT EXISTS \' || schema || \'.\' || partition ||
-                        \' (CONSTRAINT \' || partition || \'_pkey PRIMARY KEY (datetime, user_id, object_type),
-                          check (datetime >= DATE \'\'\' || to_char(NEW.datetime, \'YYYY-MM-01\') || \'\'\'
-                                  AND datetime < DATE \'\'\' || to_char(NEW.datetime + interval \'1 month\', \'YYYY-MM-01\') || \'\'\') ) \' ||
-                        \'INHERITS (\' || schema || \'.response_info);\';
+                  RAISE NOTICE \'A partition has been created %\',partition;
+                  EXECUTE \'CREATE TABLE IF NOT EXISTS \' || schema || \'.\' || partition ||
+                          \' (CONSTRAINT \' || partition || \'_pkey PRIMARY KEY (region_id, api, request_date, user_id, app_name, host, object_count),
+                            check (request_date >= DATE \'\'\' || to_char(NEW.request_date, \'YYYY-MM-01\') || \'\'\'
+                                    AND request_date < DATE \'\'\' || to_char(NEW.request_date + interval \'1 month\', \'YYYY-MM-01\') || \'\'\') ) \' ||
+                          \'INHERITS (\' || schema || \'.requests_calls);\';
                 END IF;
-                EXECUTE \'INSERT INTO \' || schema || \'.\' || partition || \' SELECT(\' || schema || \'.response_info\' || \' \' || quote_literal(NEW) || \').*;\';
+                EXECUTE \'INSERT INTO \' || schema || \'.\' || partition || \' SELECT(\' || schema || \'.requests_calls\' || \' \' || quote_literal(NEW) || \').*;\';
                 RETURN NULL;
-            END;
-            $$
+              END;
+              $$
             LANGUAGE plpgsql;
         ');
-
-        $this->addSql('
-            CREATE TRIGGER insert_response_info_trigger
-                BEFORE INSERT ON stat_compiled.response_info
-                FOR EACH ROW EXECUTE PROCEDURE response_info_insert_trigger();
-        ');
-
-        $this->addSql('GRANT SELECT ON stat_compiled.response_info TO usrsql_nmp_stat;');
     }
 
     /**
@@ -67,7 +49,30 @@ class Version20151016185153 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        $this->addSql('DROP TABLE IF EXISTS stat_compiled.response_info CASCADE;');
-        $this->addSql('DROP FUNCTION IF EXISTS response_info_insert_trigger();');
+        $this->addSql('ALTER TABLE stat_compiled.requests_calls DROP COLUMN object_count;');
+
+        $this->addSql('
+          CREATE OR REPLACE FUNCTION requests_calls_insert_trigger()
+            RETURNS trigger AS $$
+              DECLARE
+                schema VARCHAR(100);
+                partition VARCHAR(100);
+              BEGIN
+                schema := \'stat_compiled\';
+                partition := \'requests_calls\' || \'_\' || to_char(NEW.request_date, \'"y"YYYY"m"MM\');
+                IF NOT EXISTS(SELECT 1 FROM pg_tables WHERE tablename=partition and schemaname=schema) THEN
+                  RAISE NOTICE \'A partition has been created %\',partition;
+                  EXECUTE \'CREATE TABLE IF NOT EXISTS \' || schema || \'.\' || partition ||
+                          \' (CONSTRAINT \' || partition || \'_pkey PRIMARY KEY (region_id, api, request_date, user_id, app_name),
+                            check (request_date >= DATE \'\'\' || to_char(NEW.request_date, \'YYYY-MM-01\') || \'\'\'
+                                    AND request_date < DATE \'\'\' || to_char(NEW.request_date + interval \'1 month\', \'YYYY-MM-01\') || \'\'\') ) \' ||
+                          \'INHERITS (\' || schema || \'.requests_calls);\';
+                END IF;
+                EXECUTE \'INSERT INTO \' || schema || \'.\' || partition || \' SELECT(\' || schema || \'.requests_calls\' || \' \' || quote_literal(NEW) || \').*;\';
+                RETURN NULL;
+              END;
+              $$
+            LANGUAGE plpgsql;
+        ');
     }
 }
