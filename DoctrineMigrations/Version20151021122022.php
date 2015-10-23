b9ac508d940430f7646cad4fc400dc94fd1bc9a0
Rémy Abi-Khalil <remyabi@gmail.com>
2015-10-23 15:44:58 +0200

    Indexes implemented in 20151021122022 migration

--- DoctrineMigrations/Version20151021122022.php
+++ DoctrineMigrations/Version20151021122022.php
@@ -20,6 +20,54 @@ class Version20151021122022 extends AbstractMigration
 
         // requests_calls_pkey removed. (Duplicate values are presents)
         $this->addSql('ALTER TABLE stat_compiled.requests_calls DROP CONSTRAINT requests_calls_pkey;');
+
+        // requests_calls_indexes added.
+        $this->addSql('CREATE INDEX requests_calls_region_id_api_request_date_idx ON stat_compiled.requests_calls (region_id, api, request_date);');
+        $this->addSql('CREATE INDEX requests_calls_user_id_request_date_idx ON stat_compiled.requests_calls (user_id, request_date);');
+        $this->addSql('CREATE INDEX requests_calls_end_point_id_request_date_idx ON stat_compiled.requests_calls (end_point_id, request_date);');
+
+        // New version of the partition auto-creation for requests_calls
+        $this->addSql('
+          CREATE OR REPLACE FUNCTION requests_calls_insert_trigger()
+            RETURNS trigger AS $$
+              DECLARE
+                schema VARCHAR(100);
+                partition VARCHAR(100);
+              BEGIN
+                schema := \'stat_compiled\';
+                partition := \'requests_calls\' || \'_\' || to_char(NEW.request_date, \'"y"YYYY"m"MM\');
+                IF NOT EXISTS(SELECT 1 FROM pg_tables WHERE tablename=partition and schemaname=schema) THEN
+                  RAISE NOTICE \'A partition has been created %\',partition;
+                  EXECUTE \'CREATE TABLE IF NOT EXISTS \' || schema || \'.\' || partition ||
+                          \' (check (request_date >= DATE \'\'\' || to_char(NEW.request_date, \'YYYY-MM-01\') || \'\'\'
+                              AND request_date < DATE \'\'\' || to_char(NEW.request_date + interval \'1 month\', \'YYYY-MM-01\') || \'\'\') ) \' ||
+                          \'INHERITS (\' || schema || \'.requests_calls);\';
+                  EXECUTE \'CREATE INDEX \' || partition || \'_region_id_api_request_date_idx ON \' || schema || \'.\' || partition || \' (region_id, api, request_date);\';
+                  EXECUTE \'CREATE INDEX \' || partition || \'_user_id_request_date_idx ON \' || schema || \'.\' || partition || \' (user_id, request_date);\';
+                  EXECUTE \'CREATE INDEX \' || partition || \'_end_point_id_request_date_idx ON \' || schema || \'.\' || partition || \' (end_point_id, request_date);\';
+                END IF;
+                EXECUTE \'INSERT INTO \' || schema || \'.\' || partition || \' SELECT(\' || schema || \'.requests_calls\' || \' \' || quote_literal(NEW) || \').*;\';
+                RETURN NULL;
+              END;
+              $$
+            LANGUAGE plpgsql;
+        ');
+
+        // Update pkey on all existing partitions of requests_calls
+        $this->addSql('
+            DO $$DECLARE
+              requests_calls_partitions CURSOR FOR SELECT tablename FROM pg_tables WHERE tablename like \'requests_calls_%\' and schemaname=\'stat_compiled\' ORDER BY tablename;
+            BEGIN
+              RAISE NOTICE \'Starting ...\';
+              FOR partition IN requests_calls_partitions LOOP
+                RAISE NOTICE \'Partition: %s ...\', quote_ident(partition.tablename);
+                EXECUTE \'ALTER TABLE stat_compiled.\' || partition.tablename || \' DROP CONSTRAINT \' || partition.tablename || \'_pkey;\';
+                EXECUTE \'CREATE INDEX \' || partition.tablename || \'_region_id_api_request_date_idx ON stat_compiled.\' || partition.tablename || \' (region_id, api, request_date);\';
+                EXECUTE \'CREATE INDEX \' || partition.tablename || \'_user_id_request_date_idx ON stat_compiled.\' || partition.tablename || \' (user_id, request_date);\';
+                EXECUTE \'CREATE INDEX \' || partition.tablename || \'_end_point_id_request_date_idx ON stat_compiled.\' || partition.tablename || \' (end_point_id, request_date);\';
+              END LOOP;
+            END$$;
+        ');
     }
 
     /**
@@ -66,11 +114,21 @@ class Version20151021122022 extends AbstractMigration
                 RAISE NOTICE \'Partition: %s ...\', quote_ident(partition.tablename);
                 EXECUTE \'CREATE UNIQUE INDEX \' || partition.tablename || \'_add_host_id_temp_idx ON stat_compiled.\' || partition.tablename || \' (region_id, api, request_date, user_id, app_name, host);\';
                 EXECUTE \'ALTER TABLE stat_compiled.\' || partition.tablename || \' ADD CONSTRAINT \' || partition.tablename || \'_pkey PRIMARY KEY USING INDEX \' || partition.tablename || \'_add_host_id_temp_idx;\';
+                EXECUTE \'DROP INDEX stat_compiled.\' || partition.tablename || \'_region_id_api_request_date_idx;\';
+                EXECUTE \'DROP INDEX stat_compiled.\' || partition.tablename || \'_user_id_request_id_idx;\';
+                EXECUTE \'DROP INDEX stat_compiled.\' || partition.tablename || \'_end_point_id_request_id_idx;\';
               END LOOP;
             END$$;
         ');
 
+        // requests_calls_indexes added.
+        $this->addSql('DROP INDEX requests_calls_region_id_api_request_date_idx;');
+        $this->addSql('DROP INDEX requests_calls_user_id_request_date_idx;');
+        $this->addSql('DROP INDEX requests_calls_end_point_id_request_date_idx;');
+
         // Remove end_point_id column from stat_compiled.requests_calls table
         $this->addSql('ALTER TABLE stat_compiled.requests_calls DROP COLUMN end_point_id;');
+
+
     }
 }
